cc0bfa22d6679fa73d8ddd05c385147f42b34b1f
ukjunior <devmagnusuche@gmail.com>
2022-06-25 12:28:13 -0700

    update my expenses-chart-component

--- script.js
+++ script.js
@@ -1,65 +1,44 @@
-
-//import the data.json file
+/* <div class="chart">
+<div class="title">7878</div>
+<div class="column"></div>
+<div class="days">mon</div>
+</div> */
 import data from './data.json' assert{ type: "json"};
 
-// console.log(data);
+window.onload = () => {
+   
+  //get reference from chartstick
+  let chartStick = document.querySelector('.chart-stick');
+  let index = 0;
+  let max = 0;
+  data.forEach((item, idx) => {
 
+    if(item.amount > max){
+      max = item.amount;
+      index = idx;
+    }
 
-// const data = fetch('data.json')
-// .then(response => response.json())
-// .then(data =>  {
-//     console.log(data);
-//     return data
-// }
-// )
-// .catch(error => console.log(error));
-// console.log('texting');
-//  console.log(objectToArray(data));
-// convert data into array
-  // let dataArray = Object.values(data);
-  
-let chartStick = document.querySelector('.chart-stick');
-data.forEach(function(item){
-    // console.log('item.day');
     let chart = document.createElement('div');
+    let title = document.createElement('div');
     let column = document.createElement('div');
-    let days = document.createElement('div')
+    let days = document.createElement('div');
+
     chart.classList = 'chart';
-    // let dollorsign = $;
-    chart.innerHTML = 
-    `<div class="">
-    <div class=" title active"><span>$</span>${item.amount}</div>
-    </div>`;
-    // chart.style.backgroundColor = item.color;
+    title.classList = 'title';
+    column.classList = 'column';
+    days.classList = 'days';
 
-    // divide multiply the amount by 2 and add px to make height
-    let height = (item.amount)* 2.5 + "px";
-    days.innerHTML = `${item.day}`;
-    column.style.height = `${height}`;
-    if(item.day === "wed"){
+    title.innerHTML = "$" + item.amount;
+    column.style.height = `${item.amount}` * 2.5 + 'px';
+    days.innerHTML = item.day;
     
-      column.setAttribute('class', 'active');
-    } else{
-        column.setAttribute('class', 'column');
-      }
-    chart.appendChild(column);
-    chart.append(days);
+    chart.append(title, column, days);
     chartStick.appendChild(chart);
-    // console.log(column.classList);
-    // if()
-    
   })
-  
-  
+
+  let activeChart = document.querySelectorAll('.chart')[index];
+  activeChart.classList.add('active');
 
 
-// function addTitle(){
 
-//   let newColumn = column;
-  
-//   for(let x in newColumn){
-//     console.log(x);
-//   }
-// }
-// name(column)
-      // console.log(column);
+} 
